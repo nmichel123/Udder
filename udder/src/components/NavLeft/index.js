ae8cc457e076f22e9bad675f4d2cdfa31487ad52
Nico Michel <nicomichel@utexas.edu>
2021-02-24 15:21:16 -0600

    Left Navbar complete
    Fa icons added and spacing

--- udder/src/components/NavLeft/index.js
+++ udder/src/components/NavLeft/index.js
@@ -4,15 +4,15 @@ import './style.css';
 function NavLeft () {
     return (
         <div className = "navLeft">
-                <div className = "navLeftItem" ><i class="fas fa-home fa-fw"></i>Home</div>
-                <div className = "navLeftItem" >Explore</div>
-                <div className = "navLeftItem" >Notifications</div>
-                <div className = "navLeftItem" >Messages</div>
-                <div className = "navLeftItem" >Bookmarks</div>
-                <div className = "navLeftItem" >Lists</div>
-                <div className = "navLeftItem" >Profile</div>
-                <div className = "navLeftItem" >More</div>
-                <div className = "udderButton" >Udder</div>
+                <div className = "navLeftItem" ><i class="fas fa-home fa-lg fa-fw"></i>   Home</div>
+                <div className = "navLeftItem" ><i class="fas fa-hashtag fa-lg fa-fw"></i>  Explore</div>
+                <div className = "navLeftItem" ><i class="far fa-bell fa-lg fa-fw"></i>    Notifications</div>
+                <div className = "navLeftItem" ><i class="far fa-envelope fa-lg fa-fw"></i>    Messages</div>
+                <div className = "navLeftItem" ><i class="far fa-bookmark fa-lg fa-fw"></i>    Bookmarks</div>
+                <div className = "navLeftItem" ><i class="fas fa-list fa-lg fa-fw"></i>    Lists</div>
+                <div className = "navLeftItem" ><i class="far fa-user fa-lg fa-fw"></i>    Profile</div>
+                <div className = "navLeftItem" ><i class="far fa-circle fa-lg fa-fw"></i>    More</div>
+                <button className = "udderButton" style = {{borderRadius: '25px'}}>Udder</button>
         </div>
     );
 }
