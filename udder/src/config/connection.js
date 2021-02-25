58da54427fa7bf0d2fec98332c44cebad43a294a
Nico Michel <nicomichel@utexas.edu>
2021-02-25 15:56:00 -0600

    Sql connection created

--- udder/src/config/connection.js
+++ udder/src/config/connection.js
@@ -4,6 +4,16 @@ var connection = mysql.createConnection({
     port: 3306,
     host: "localhost",
     user: "root",
-    password: "",
-    database: ""
-})
+    password: "sanctioned",
+    database: "Udder"
+});
+
+connection.connect(function(err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
