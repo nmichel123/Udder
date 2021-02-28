e6e19852449ebd47932e2797dd1d03f767342abc
Nico Michel <nicomichel@utexas.edu>
2021-02-28 00:11:56 -0600

    Changing directions with textbox as it requires typescript
    tsx

--- udder/src/config/connection.js
+++ udder/src/config/connection.js
@@ -5,7 +5,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "sanctioned",
-    database: "Udder"
+    database: "udder"
 });
 
 connection.connect(function(err) {
