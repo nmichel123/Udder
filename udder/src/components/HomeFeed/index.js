d86065de2c45465c9b5e47fe042c66e394d23397
Nico Michel <nicomichel@utexas.edu>
2021-11-28 19:19:22 -0600

    Added code to mount udders from db
    Throwing errors now the POST request is no longer working

--- udder/src/components/HomeFeed/index.js
+++ udder/src/components/HomeFeed/index.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, {Component} from 'react'
+import UdderDataService from '../../services/udder.service'
 import './style.css'
 
-function HomeFeed () {
+class HomeFeed extends Component {
+    constructor(props) {
+        super(props);
+        this.retrieveUdders = this.retrieveUdders.bind(this);
+    }
+
+    componentDidMount() {
+        this.retrieveUdders(); 
+    }
+
+    retrieveUdders() {
+        UdderDataService.getAll()
+            .then(response => {
+                this.setState({
+                    udders:response.data
+                });
+                console.log(response.data)
+            })
+            .catch(e => {
+                console.log(e);
+            })
+    }
+
+
+
+    render() {
     return (
         <div className = "homeFeed">
         </div>
     )
+    }
 }
 
 export default HomeFeed
