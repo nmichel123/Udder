9225807042f73dc04bfac8d5380cc28ebde83943
Nico Michel <nicomichel@utexas.edu>
2022-11-07 23:48:53 -0600

    Fixed

--- udder/src/components/NewsFeed/index.js
+++ udder/src/components/NewsFeed/index.js
@@ -1,31 +1,33 @@
 import React from 'react';
 import './style.css';
 
+const NewsAPI = process.env.REACT_APP_NewsAPI
 
 class NewsFeed extends React.Component {
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
         this.state = {
             news: [],
-        }
+        };
     }
 
     componentDidMount () {
-        fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${MY_KEY}`)
-        .then(res=> res.json())
+        fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${NewsAPI}`)
+        .then(res => res.json())
         .then((data) => {
-            this.setState({ news: data})
+            this.setState({ news: data })
         })
     .catch(console.log)
     }
     render() {
+        const {data} = this.state
         return (
             <div className='NewsBox'>
-                {this.state.news.map((news) => (
+                {data && data.map((articles) => (
                     <div className='card'>
                         <div className='card-body'>
-                            <h5 className='newsTitle'>{news.title}</h5>
-                            <img src={news.urlToImage} alt={news.title} />
+                            <h5 className='newsTitle'>{articles.title}</h5>
+                            <img src={articles.urlToImage} alt={articles.title} />
                         </div>
                     </div>
                 ))}
