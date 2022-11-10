c57287a879100ade478bdf9e9db84bf43615a964
Nico Michel <nicomichel@utexas.edu>
2022-11-09 19:27:26 -0600

    ? in map solves cannot read properties of undefined problem

--- udder/src/components/NewsFeed/index.js
+++ udder/src/components/NewsFeed/index.js
@@ -1,29 +1,37 @@
 import React from 'react';
 import './style.css';
-import $ from 'jquery';
 
 const NewsAPI = process.env.REACT_APP_NEWS_API
 
 class NewsFeed extends React.Component {
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
         this.state = {
+            isLoaded: false,
             news: [],
         };
     }
 
+
+
     componentDidMount () {
-        fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${NewsAPI}`)
+        fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=557f4c9f7e9641c08f0c7d7370a671f0`)
         .then(res => res.json())
         .then((data) => {
-            this.setState({ news: data })
+            this.setState({ 
+                isLoaded: true,
+                news: data.news 
+            });
+            console.log(data.news)
         })
-    .catch(console.log)
+    .catch(e =>{
+        console.log(e)
+    })
     }
     render() {
         return (
             <div className='NewsBox'>
-                {this.props.data.map((articles) => (
+                {this.state.news?.map((articles) => (
                     <div className='card'>
                         <div className='card-body'>
                             <h5 className='newsTitle'>{articles.title}</h5>
